cf867708e539bf2531807340081ec2938ea081ba
Neelabh Paul <npaul_be19@thapar.edu>
2023-08-13 16:25:20 +0530

    folder structure & required files created.

---
+++ components/Nav.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Link from "next/link"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+
+const Nav = () => {
+    return (
+        <div>
+            nav
+        </div>
+    )
+}
+
+export default Nav
