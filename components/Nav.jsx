fdfc177aee7a5ec47175912588822b8d939eb960
Neelabh Paul <npaul_be19@thapar.edu>
2023-08-13 18:41:58 +0530

    navbar frontend created

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,10 +6,136 @@ import { useEffect, useState } from "react"
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
+    const isUserLoggedIn = true
+    const [providers, setProviders] = useState(null)
+    const [toggleDropdown, setToggleDropdown] = useState(false)
+
+    useEffect(() => {
+        const setProviders = async () => {
+            const response = await getProviders()
+            setProviders(response)
+        }
+        setProviders()
+    }, [])
+
     return (
-        <div>
-            nav
-        </div>
+        <nav className="flex-between w-full mb-16 pt-3">
+            <Link href='/' className="flex flex-center gap-2">
+                <Image
+                    width={30}
+                    height={30}
+                    className="object-contain"
+                    src='/assets/images/logo.svg'
+                />
+                <p className="logo_text">Promptopia</p>
+            </Link>
+            {/* desktop navigation */}
+            <div className="sm:flex hidden">
+                {
+                    isUserLoggedIn ?
+                        <div className="flex gap-3 md:gap-5">
+                            <Link href={'/create-prompt'} className="black_btn">
+                                Create
+                            </Link>
+                            <button type="button" onClick={signOut} className="outline_btn">
+                                Sign Out
+                            </button>
+                            <Link href={'/profile'}>
+                                <Image
+                                    src="/assets/images/logo.svg"
+                                    className="rounded-full"
+                                    width={37}
+                                    height={37}
+                                    alt="profile"
+                                />
+                            </Link>
+                        </div>
+                        :
+                        <>
+                            {
+                                providers &&
+                                Object.values(providers).map((provider) => {
+                                    return (
+                                        <button
+                                            type="button"
+                                            key={provider.name}
+                                            onClick={() => signIn(provider.id)}
+                                            className="black_btn"
+                                        >
+                                            Sign In
+                                        </button>
+                                    )
+                                })
+                            }
+                        </>
+                }
+            </div>
+            {/* mobile navigation */}
+            <div className="sm:hidden flex relative">
+                {
+                    isUserLoggedIn ?
+                        <div className="flex">
+                            <Image
+                                src="/assets/images/logo.svg"
+                                className="rounded-full"
+                                width={37}
+                                height={37}
+                                alt="profile"
+                                onClick={() => setToggleDropdown((prev) => !prev)}
+                            />
+                            {
+                                toggleDropdown &&
+                                (
+                                    <div className="dropdown">
+                                        <Link
+                                            href={'/profile'}
+                                            className="dropdown_link"
+                                            onClick={()=> setToggleDropdown(false)}
+                                        >
+                                            My profile
+                                        </Link>
+                                        <Link
+                                            href={'/create-prompt'}
+                                            className="dropdown_link"
+                                            onClick={()=> setToggleDropdown(false)}
+                                        >
+                                            Create 
+                                        </Link>
+                                        <button
+                                            type="button"
+                                            onClick={()=>{
+                                                setToggleDropdown(false)
+                                                signOut()
+                                            }}
+                                            className="mt-5 w-full black_btn"
+                                        >
+                                            Sign Out
+                                        </button>
+                                    </div>
+                                )
+                            }
+                        </div>
+                        :
+                        <>
+                            {
+                                providers &&
+                                Object.values(providers).map((provider) => {
+                                    return (
+                                        <button
+                                            type="button"
+                                            key={provider.name}
+                                            onClick={() => signIn(provider.id)}
+                                            className="black_btn"
+                                        >
+                                            Sign In
+                                        </button>
+                                    )
+                                })
+                            }
+                        </>
+                }
+            </div>
+        </nav>
     )
 }
 
